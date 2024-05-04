0ad5854dda40c3be42da5522593a2940ebf1a193
siapar <makssiatecki44@gmail.com>
2024-05-04 12:04:09 +0200

    dodane

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import company.beans.Person;
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -17,8 +19,20 @@ public class Main {
         }
 
         EmailMessenger emailMessenger = new EmailMessenger();
+        Person[] people = new Person[5];
 
         String mathResult = String.valueOf(MathUtils.add(2, 2));
         emailMessenger.sendMessage(mathResult);
+        final int b = 10;
+        Random random = new Random();
+        for (var personIndex = 0; personIndex < people.length; personIndex++) {
+            try {
+                people[personIndex] = new Person("John Doe", 10 + random.nextInt(5, 20) * 7);
+                String ageResult = String.valueOf(MathUtils.add(people[personIndex].getAge(), b));
+                emailMessenger.sendMessage(ageResult);
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
     }
 }
